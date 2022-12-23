1b0f1e881fa206f421079e40b83cdb60db2e89e7
Vatsal Harlalka <harlalka.vatsal@gmail.com>
2022-12-23 20:37:22 +0530

    btn changes

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+// console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button Clicked');
+    modal.classList.remove('hidden'); //. is not used with class name here
+    overlay.classList.remove('hidden');
+  });
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
