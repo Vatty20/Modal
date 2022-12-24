c3929c26a5766c6889e31243909a9960772f51de
Vatsal Harlalka <harlalka.vatsal@gmail.com>
2022-12-24 11:56:59 +0530

    code refactoring

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 // console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    modal.classList.remove('hidden'); //. is not used with class name here
-    overlay.classList.remove('hidden');
-  });
+const openModal = function () {
+  //   console.log('Button Clicked');
+  modal.classList.remove('hidden'); //. is not used with class name here
+  overlay.classList.remove('hidden');
+};
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
